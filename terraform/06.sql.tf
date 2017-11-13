d39dbabebd5ebf09924e54c1a93686b684e6864d
Jay Kim <rpkim.jay@gmail.com>
2017-11-13 17:34:04 +0900

    add

--- terraform/06.sql.tf
+++ terraform/06.sql.tf
@@ -11,11 +11,13 @@ resource "google_sql_database_instance" "master" {
     ip_configuration {
         authorized_networks = {
             name = "${google_compute_instance.vm1.name}"
-            value = "${google_compute_instance.vm1.network_interface.0.address}/32"
+            value = "${google_compute_instance.vm1.network_interface.0.access_config.0.assigned_nat_ip}/32"
+            //value = "${google_compute_instance.vm1.network_interface.0.address}/32"
         }
         authorized_networks = {
             name = "${google_compute_instance.vm2.name}"
-            value = "${google_compute_instance.vm2.network_interface.0.address}/32"
+            value = "${google_compute_instance.vm2.network_interface.0.access_config.0.assigned_nat_ip}/32"
+            //value = "${google_compute_instance.vm2.network_interface.0.address}/32"
         }
     }
   }
